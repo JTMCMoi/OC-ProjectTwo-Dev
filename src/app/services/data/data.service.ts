e4205c95fc5364f80f0d7d73c1fe6ff633905305
JTMCMoi <jtm.corpo@gmail.com>
2026-01-14 06:24:55 +0100

    code data states

--- src/app/services/data/data.service.ts
+++ src/app/services/data/data.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable,shareReplay,catchError,throwError } from 'rxjs';
+import { Observable,shareReplay,catchError,throwError,BehaviorSubject, mergeMap, of, delay } from 'rxjs';
 import { Olympic } from 'src/app/models/olympic';
 
 @Injectable({
@@ -9,6 +9,8 @@ import { Olympic } from 'src/app/models/olympic';
 export class DataService {
   private url = './assets/mock/olympic.json';
   private olympics$! : Observable<Olympic[]>;
+  private stateSubject = new BehaviorSubject<string>('loading');
+  public readonly state$ = this.stateSubject.asObservable();
 
   constructor(private http: HttpClient) { }
 
@@ -16,11 +18,32 @@ export class DataService {
 
     if ( !this.olympics$ )
     {
-      //Emmet une seule fois et ne nécessite donc pas de gestion de l'observable
+      this.stateSubject.next('loading');
+      console.log('data: loading');
+
       this.olympics$ = this.http.get<Olympic[]>(this.url).pipe(
+        mergeMap(data => {
+          if (data.length === 0) {
+            this.stateSubject.next('empty');
+            console.log('data: empty');
+            return throwError(() => new Error('Erreur on getting datas (empty)'));
+          }
+          return of(data);
+        }),
         shareReplay(1),
-        catchError((r:HttpErrorResponse) => throwError(() => new Error(`Erreur on getting datas (${r.message})`)))
+        delay(1500),
+        catchError((r:HttpErrorResponse) => {
+          this.stateSubject.next('error');
+          console.log('data: error');
+          return throwError(() => new Error(`Erreur on getting datas (${r.message})`));
+        })
       );
+
+      if ( this.stateSubject.value !== 'empty' && this.stateSubject.value !== 'error' )
+      {
+        this.stateSubject.next('ready');
+        console.log('data: ready');
+      }
     }
 
     return this.olympics$;
